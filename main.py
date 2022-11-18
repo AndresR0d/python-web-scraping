f8108877b93922d151f94adf9b166a06fe654512
Andres Rodriguez Rosas <79877748+rodrosas@users.noreply.github.com>
2022-11-18 16:10:49 -0600

    Update main.py
    Wrapped the script inside a function so it can be called.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from cgitb import html
-from xml.dom.minidom import Element
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
@@ -8,31 +7,38 @@ import pandas as pd
 data = requests.get('http://www.ssn.unam.mx/sismicidad/ultimos/').text
 #Creating BeautifulSoupObject
 soup = BeautifulSoup(data, 'lxml')
+
+'''
 #Veryfing tables and their clasess
 print('Classes of each table')
 for table in soup.find_all('table'):
     print(table.get('class'))
-#Storing table in variable
-earthquake_table = soup.find('table', class_ = 'table-condensed')
+'''
 
-#Creating a list to store headers
-headers=[]
+def main(data, soup):
+    #Storing table in variable
+    earthquake_table = soup.find('table', class_ = 'table-condensed')
 
-#Iterating over earthquake_table to find all the headers 
-for i in earthquake_table.find_all('th'):
-    title = i.text.strip()
-    headers.append(title)
-print(headers)
+    #Creating a list to store headers
+    headers=[]
 
-#Creating data frame where the headers are assigned as columns
-df = pd.DataFrame(columns = headers)
-print(df)
+    #Iterating over earthquake_table to find all the headers 
+    for i in earthquake_table.find_all('th'):
+        title = i.text.strip()
+        headers.append(title)
+    print(headers)
 
-for row in earthquake_table.find_all('tr')[1:]:
-    data = row.find_all('td')
-    row_data = [td.text.strip() for td in data]
-    length = len(df)
-    df.loc[length] = row_data
+    #Creating data frame where the headers are assigned as columns
+    df = pd.DataFrame(columns = headers)
     print(df)
 
-df.to_csv('terremoto.csv', encoding='utf-8')
+    for row in earthquake_table.find_all('tr')[1:]:
+        data = row.find_all('td')
+        row_data = [td.text.strip() for td in data]
+        length = len(df)
+        df.loc[length] = row_data
+        print(df)
+
+    return df.to_csv('terremoto.csv', encoding='utf-8')
+
+main(data, soup)
